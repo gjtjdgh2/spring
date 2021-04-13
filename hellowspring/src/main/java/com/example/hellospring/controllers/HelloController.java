aebe25568e7b06b62d22ad979a5217a42632721c
gjtjdgh <gjtjdgh2@naver.com>
2021-04-13 14:11:33 +0900

    첫번째 spring 요청 처리

---
+++ hellowspring/src/main/java/com/example/hellospring/controllers/HelloController.java
@@ -0,0 +1,25 @@
+package com.example.hellospring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+	//요청 받아야한다
+	//url패턴 /hello에 반응하고
+	//파라메터로 name을 받는다
+	//예: /hello?name=홍길동
+	@RequestMapping("/hello")
+	public ModelAndView hello(@RequestParam String name) {
+		//Spring의 기본 응답 객체는 ModelAndView
+		//데이터의 뷰 객체의 정보를 함깨 가지고 있다
+		ModelAndView mav = new ModelAndView();
+		
+		//데이터를 설정
+		mav.addObject("message","Hello"+name);
+		mav.setViewName("/WEB-INF/views/hello.jsp");
+		return mav;
+	}
+}
